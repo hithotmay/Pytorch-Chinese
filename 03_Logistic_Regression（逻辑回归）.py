105c36f9bf00c1cf3fc40864eae2e2dc16ee56ca
hithotmay <53128214+hithotmay@users.noreply.github.com>
2020-12-20 14:26:49 +0800

    Create 03_Logistic_Regression（逻辑回归）.py

---
+++ 03_Logistic_Regression（逻辑回归）.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+
+# Hyper-parameters 
+# 超参数
+input_size = 28 * 28    # 784
+num_classes = 10
+num_epochs = 5
+batch_size = 100
+learning_rate = 0.001
+
+# MNIST dataset (images and labels)
+# MNIST  数据集（图像和标签）
+train_dataset = torchvision.datasets.MNIST(root='../../data', 
+                                           train=True, 
+                                           transform=transforms.ToTensor(),
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='../../data', 
+                                          train=False, 
+                                          transform=transforms.ToTensor())
+
+# Data loader (input pipeline)
+# 数据加载器（输入管道）
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                          batch_size=batch_size, 
+                                          shuffle=False)
+
+# Logistic regression model
+# 逻辑回归模型
+model = nn.Linear(input_size, num_classes)
+
+# Loss and optimizer
+# 损失函数和优化器
+# nn.CrossEntropyLoss() computes softmax internally
+# 交叉熵损失
+criterion = nn.CrossEntropyLoss()  
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  
+
+# Train the model
+# 训练模型
+total_step = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # Reshape images to (batch_size, input_size)
+        # 将图像重塑到 （batch_size，input_size）
+        images = images.reshape(-1, input_size)
+        
+        # Forward pass
+        # 前向传播
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        
+        # Backward and optimize
+        # 反向传播并优化
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if (i+1) % 100 == 0:
+            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+# Test the model
+# 测试模型
+# In test phase, we don't need to compute gradients (for memory efficiency)
+# 在测试阶段，我们不需要计算梯度（为了内存效率）
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, input_size)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
+
+    print('Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+
+# Save the model checkpoint
+# 保存模型检查点
+torch.save(model.state_dict(), 'model.ckpt')
